9dcd52182c9f71bb19497a83b47b78b6d6db1b64
hifito <himma29@gmail.com>
2022-01-13 20:03:22 +0700

    first commit

--- src/helpers/format/thousand.js
+++
@@ -1,7 +0,0 @@
-/* eslint no-extend-native: ["error", {"exceptions": ["Number"]}] */
-if (!Number.hasOwnProperty("thousand"))
-  Number.prototype.thousand = function (decimals = 0) {
-    const thousand = new Intl.NumberFormat(navigator.language);
-
-    return thousand.format(this);
-  };
